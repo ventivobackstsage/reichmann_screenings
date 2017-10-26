347ebe5c132aff3a21c2b5419f0b8328cc3d6e29
Dan Petrescu <petrescuvdan@gmail.com>
2017-10-26 22:20:22 +0300

    impersonate

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -100,7 +100,7 @@ class UsersController extends JoshController
                 if(!Sentinel::inRole('admin')) return "";
                 $actions = '<a href='. route('admin.users.edit', $user->id) .'><i class="livicon" data-name="edit" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title="update user"></i></a>';
                 if ((Sentinel::getUser()->id != $user->id) && ($user->id != 1)) {
-                    $actions .= '<a href='. route('admin.users.confirm-delete', $user->id) .' data-toggle="modal" data-target="#delete_confirm"><i class="livicon" data-name="user-remove" data-size="18" data-loop="true" data-c="#f56954" data-hc="#f56954" title="impersonate user"></i></a><a href='. route('admin.users.impersonate', $user->id) .'><i class="livicon" data-name="ghost" data-size="18" data-loop="true" data-c="#f56954" data-hc="#f56954" title="delete user"></i></a>';
+                    $actions .= '<a href='. route('admin.users.confirm-delete', $user->id) .' data-toggle="modal" data-target="#delete_confirm"><i class="livicon" data-name="user-remove" data-size="18" data-loop="true" data-c="#f56954" data-hc="#f56954" title="delete user"></i></a><a href='. route('admin.users.impersonate', $user->id) .'><i class="livicon" data-name="ghost" data-size="18" data-loop="true" data-c="#00bc8c" data-hc="#f56954" title="impersonate user"></i></a>';
                 }
                 return $actions;
             })
